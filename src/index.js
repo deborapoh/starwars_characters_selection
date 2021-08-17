8ef804a15e186ebbdc258fdb9dcb0b86f2cba419
Debora de Oliveira <deborapoh@gmail.com>
2021-08-17 00:31:38 -0300

    refactor: apply new routers

--- src/index.js
+++ src/index.js
@@ -1,38 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import Router from './router'
 
-import PaginaNome from './components/PaginaNome';
-import PaginaClasse from './components/PaginaClasse';
-import Jedi from './components/Jedi';
-import Sith from './components/Sith';
-import Luke from './components/Personagem/Luke';
-import Leia from './components/Personagem/Leia';
-import Beru from './components/Personagem/Beru';
-import Darth from './components/Personagem/Darth';
-
-import registerServiceWorker from './registerServiceWorker';
+import './index.css'
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
+import registerServiceWorker from './registerServiceWorker';
+
 ReactDOM.render(
-
-    <BrowserRouter>
-        <Switch>
-            <Route path="/" exact={true} component={App} />
-            <Route path="/pagina_nome" component={PaginaNome} />
-            <Route path="/pagina_classe" component={PaginaClasse} />
-            <Route path="/luke" component={Luke} />
-            <Route path="/leia" component={Leia} />
-            <Route path="/beru" component={Beru} />
-            <Route path="/darth" component={Darth} />
-            <Route path="/jedi" component={Jedi} />
-            <Route path="/sith" component={Sith} />
-        </Switch>
-    </BrowserRouter>
-    
-    , document.getElementById('root')
-
+  <BrowserRouter>
+    <Router />
+  </BrowserRouter>,
+  document.getElementById("root")
 );
 
 registerServiceWorker();
