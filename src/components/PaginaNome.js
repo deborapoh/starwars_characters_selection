f2f50f8fe49813180feb22a51a759a192f9446f6
Debora de Oliveira <deborapoh@gmail.com>
2021-08-17 00:33:55 -0300

    refactor: remove body from html

--- src/components/PaginaNome.js
+++ src/components/PaginaNome.js
@@ -2,49 +2,44 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 import './PaginaNome.css';
-import logo from '../logo.png';
-
-import Darth from './Personagem/Darth';
+import logo from '../assets/logo.png';
 
 class PaginaNome extends Component {
 
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            campo_nome: '',
-            genero: ''
-        };
-    }
+    this.state = {
+      campo_nome: '',
+      genero: ''
+    };
+  }
 
-    render() {
-        return (
-            <div className="Nome">
+  render() {
+    return (
+      <div className="Nome">
+        <img src={logo} className="Nome-logo" alt="logo" />
 
-                <body>
-                    <img src={logo} className="Nome-logo" alt="logo" />
+        <p>Crie um nome para o seu personagem</p>
 
-                    <p>Crie um nome para o seu personagem</p>
+        <input
+          className="Input"
+          type="text"
+          name="campo_nome"
+          onChange={(nomeCampo) => this.setState({ campo_nome: nomeCampo.value })}
+        />
 
-                    <input
-                        className="Input"
-                        type="text"
-                        name="campo_nome"
-                        onChange={ (nomeCampo) => this.setState({ campo_nome: nomeCampo.value }) }
-                    />
+        <form action="" className="genero">
+          <input type="radio" name="gender" value="female" /> Mulher<br />
+          <input type="radio" name="gender" value="male" /> Homem<br />
+        </form>
 
-                    <form action="" className="genero">
-                        <input type="radio" name="gender" value="female" /> Mulher<br />
-                        <input type="radio" name="gender" value="male" /> Homem<br />
-                    </form>
-
-                    <Link to="/pagina_classe">
-                        <button className="botaoClasse" onClick={() => console.log('') }>Próximo</button>
-                    </Link>
-                </body>
-            </div>
-        );
-    }
+        <Link to="/pagina_classe">
+          <button className="botaoClasse" >Próximo</button>
+        </Link>
+      </div>
+    );
+  }
 }
 
 export default PaginaNome;
