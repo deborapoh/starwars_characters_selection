f9da4f88e4c36f084ef935c07c177628e9c12d34
Debora de Oliveira <deborapoh@gmail.com>
2021-08-17 00:30:59 -0300

    refactor: create router constants

---
+++ src/config/constants.js
@@ -0,0 +1,17 @@
+/**
+ * App constants
+ */
+
+export default {
+  ROUTES: {
+    HOME: "/",
+    PAGINA_NOME: "/pagina_nome",
+    PAGINA_CLASSE: "/pagina_classe",
+    LUKE: "/luke",
+    LEIA: "/leia",
+    BERU: "/beru",
+    DARTH: "/darth",
+    JEDI: "/jedi",
+    SITH: "/sith"
+  }
+};
